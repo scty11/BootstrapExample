59c568c2818cce72e10a7ae139f4661f4733ee1d
scty11 <scotty_wright@my.open.ac.uk>
2014-12-05 01:46:46 +0000

    added project

---
+++ mybuild/mybuild/js/site.js
@@ -0,0 +1,36 @@
+﻿(function () {
+
+    var $pickButton = $("#pickButton");
+    
+    $("#reasonDropdown li a").on("click", function () {
+        var reason = $(this).text();
+        $pickButton.text(reason);
+    });
+
+    var $sentDialog = $("#sentDialog");
+
+    $("#contactForm").on("submit", function () {
+        $sentDialog.modal('show');
+        return false;
+    });
+
+    $("#contactForm input[type=submit]").tooltip({
+        delay: {
+            show: 500,
+            hide: 0
+        }
+    });
+    var $sentAlert = $("#sentAlert");
+
+    $sentDialog.on("hidden.bs.modal", function () {
+        //alert("close");
+        $sentAlert.show();
+    });
+
+    $sentAlert.on("close.bs.alert", function () {
+        $sentAlert.hide();
+        return false;
+    });
+
+    $("#theCarousel").carousel();
+})();
